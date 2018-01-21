c03e81721987f922598cda74123a1d6ab6e8bae3
Daniel D'Souza <daniel.dsouza5@gmail.com>
2018-01-21 10:24:35 -0700

    added readme and config support

---
+++ README.md
@@ -0,0 +1,8 @@
+# EVE Intel
+
+[![Build Status](https://travis-ci.org/daniel-dsouza/eve-bot.svg?branch=master)](https://travis-ci.org/daniel-dsouza/eve-bot)
+
+Taking the edge off intel automation.
+
+I am still updating this to work with ESI. 
+
--- bot.py
+++ bot.py
@@ -1,26 +1,41 @@
-import discord
+__version__ = 'v0.4'
+
+
+import configparser
 from discord.ext import commands
+import os
 
 from esi import common
 from esi.eve_types import System
 
 
-VERSION = 'v0.2'
-
-
 bot = commands.Bot(command_prefix='?', description='rewrite')
 common.session = bot.http.session  # is this hackish? Yes. Yes it is.
+config = configparser.ConfigParser()
 
 
 @bot.event
 async def on_ready():
     latest_semver = await common.get_latest_release()
-    if VERSION != latest_semver:
-        print("You are running version {}. Please upgrade to {}".format(VERSION, latest_semver))
+    if __version__ != latest_semver:
+        print("You are running version {}. Please upgrade to {}".format(__version__, latest_semver))
     else:
-        print("You are running version {}.".format(VERSION))
+        print("You are running version {}.".format(__version__))
 
     system = await System.new(name='BQ0-UU')
     print(system.objectID)
 
-bot.run('MjkyMzYzMzQzMzkyNjY5NzA3.DTcAxw.iNSdUsDly_daT4GSc21pX_dQ6Xk')
+if __name__ == '__main__':
+    try:
+        if not os.path.exists('eve-intel.cfg'):
+            raise FileNotFoundError
+
+        config.read('eve-intel.cfg')
+        token = config.get('discord', 'token')
+        bot.run(token)
+    except FileNotFoundError as e:
+        print('Could not find configuration file "eve-intel.cfg" in this directory.')
+    except configparser.NoSectionError or configparser.NoOptionError as e:
+        print(e)
+    finally:
+        bot.close()
